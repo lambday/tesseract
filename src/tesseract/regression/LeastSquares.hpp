38ef4a2882aff6e343528142655ed2fbc0d4ec01
lambday <heavensdevil6909@gmail.com>
2014-12-03 19:30:36 +0530

    added least square

---
+++ src/tesseract/regression/LeastSquares.hpp
@@ -0,0 +1,76 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Soumyajit De
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LEAST_SQUARES_H__
+#define LEAST_SQUARES_H__
+
+#include <tesseract/base/types.h>
+
+namespace tesseract
+{
+
+enum LeastSquareMethod
+{
+	LS_SVD,
+	LS_QR,
+	LS_NORMAL
+};
+
+template <typename T, enum LeastSquareMethod>
+struct LeastSquares;
+
+template <typename T>
+struct LeastSquares<T, LS_SVD>
+{
+	Vector<T> solve(const Matrix<T>& A, const Vector<T>& b) const
+	{
+		return A.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b);
+	}
+};
+
+template <typename T>
+struct LeastSquares<T, LS_QR>
+{
+	Vector<T> solve(const Matrix<T>& A, const Vector<T>& b) const
+	{
+		return A.colPivHouseholderQr().solve(b);
+	}
+};
+
+template <typename T>
+struct LeastSquares<T, LS_NORMAL>
+{
+	Vector<T> solve(const Matrix<T>& A, const Vector<T>& b) const
+	{
+		return (A.transpose() * A).ldlt().solve(A.transpose() * b);
+	}
+};
+
+template <typename T> using SVDLeastSquares = LeastSquares<T, LS_SVD>;
+template <typename T> using QRLeastSquares = LeastSquares<T, LS_QR>;
+template <typename T> using NormalLeastSquares = LeastSquares<T, LS_NORMAL>;
+
+}
+
+#endif // LEAST_SQUARES_H__
